326d46d27514890b070c97ecbb5fb4d50f2a4134
Yoshiaki Suzuki <suzuyoshi901@gmail.com>
2014-05-19 15:22:59 +0900

    Add Processing.cs

---
+++ PVDataProcessor/Tool/Processing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PVDataProcessor.Tool
+{
+    class Processing
+    {
+        public double CalcIntegratedWatts(PH[] PHdata, )
+        {
+            
+            return 0.0;
+        }
+    }
+}
--- PVDataProcessor/Tool/Structs.cs
+++ PVDataProcessor/Tool/Structs.cs
@@ -16,9 +16,9 @@ namespace PVDataProcessor.Tool
     }
     public class PH
     {
-        public double Voltage;
-        public double Current;
-        public double Watts;
-        public double IntegratedWatts;
+        public double Voltage;          //電圧
+        public double Current;          //電流
+        public double Watts;            //電力
+        public double IntegratedWatts;  //積算電力
     }
 }
